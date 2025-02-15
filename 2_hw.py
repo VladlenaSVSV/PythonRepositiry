6e491d5a84ef79cdfcfea0280cf44d8f635b5dac
VladlenaSVSV <vlada_pikan@mail.ru>
2025-02-15 21:05:52 +0300

    Update 2_hw.py

--- 2_hw.py
+++ 2_hw.py
@@ -1,25 +1,25 @@
 def task_1():
-    a : int = 1
-    b : float = 2,5
-    c : str = "test"
-    d : list = [1,2,3, "четыре"]
-    e : bool = True
+    my_int: int = 20                      # Тип int
+    my_float: float = 4.15                # Тип float
+    my_str: str = "Here you go"           # Тип str
+    my_list: list = [1, 2, 3, 4, 5]       # Тип list
+    my_bool: bool = True                  # Тип bool
 
-    print('тип a ', type(a))
-    print('тип b ', type(b))
-    print('тип c ', type(c))
-    print('тип d ', type(d))
-    print('тип e ', type(e))
+    print('my_int ', type(my_int))
+    print('my_float ', type(my_float))
+    print('my_str ', type(my_str))
+    print('my_list ', type(my_list))
+    print('my_bool ', type(my_bool))
 
-task_1()
+    task_1()
 
+    def task_2():
+        mylist: list = [1, 2, 3, 5, 8, 13, 21]  # ряд фибоначчи
+        print(mylist[0:3])
 
-def task_2():
-    mylist : list = [1, 2, 3, 5, 8, 13, 21] #ряд фибоначчи
-    print(mylist[0:3])
+    task_2()
 
-task_2()
+    def task_3(a: int) -> int:
+        return a * a
 
-def task_3(a : int) -> int:
-    return a * a
-print(task_3(5))
+    print(task_3(5))
